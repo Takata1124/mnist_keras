98e38aed86743dacbbb253d575f00c6e6e50df9e
takata <skkyosk1124@gmail.com>
2021-12-08 05:44:51 +0900

    first commit

--- mnist_keras/ViewController.swift
+++ mnist_keras/ViewController.swift
@@ -10,6 +10,7 @@ import CoreML
 import Vision
 
 public struct PixelData {
+    
     var a: UInt8
     var r: UInt8
     var g: UInt8
@@ -28,7 +29,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.isEditing = false
@@ -43,44 +43,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let imgSize: Int = 28
             let imageShape: CGSize = CGSize(width: imgSize, height: imgSize)
             let imagePixel = userimage.resize(to: imageShape).getPixelBuffer()
-            
-//            guard let pixeluiimage = imageFromARGB32Bitmap(pixels: imagePixel, width: 28, height: 28) else {
-//
-//                fatalError("Could not pixel")
-//            }
-            
-            //            guard let pixelcgimage: CGImage = RBGImage(data: imagePixel, width: 28, height: 28)
-            //
-            //            let pixeluiImage = UIImage(cgImage: pixelcgimage)
-            
-            var pixels = [PixelData]()
 
-            let red = PixelData(a: 255, r: 255, g: 0, b: 0)
-            let green = PixelData(a: 255, r: 0, g: 255, b: 0)
-            let blue = PixelData(a: 255, r: 0, g: 0, b: 255)
-
-            for _ in 1...900 {
-                pixels.append(red)
+            let reverseimage = imageFromARGB32Bitmap(pixels: imagePixel, width: 28, height: 28)
+            reviesedImage.image = reverseimage
+            
+            guard let reverseimage = CIImage(image: reverseimage ?? userimage) else {
+                fatalError("Could not Convert")
             }
-//            for _ in 1...300 {
-//                pixels.append(green)
-//            }
-//            for _ in 1...300 {
-//                pixels.append(blue)
-//            }
-
-            let image = imageFromARGB32Bitmap(pixels: pixels, width: 30, height: 30)
-            reviesedImage.image = image
-            
-            print(imagePixel)
-            print(type(of: imagePixel))
+            print(type(of: reverseimage))
             
             guard let ciimage = CIImage(image: userimage) else {
                 fatalError("Could not Convert")
             }
             
-            imagePrediction(image: ciimage)
+//            imagePrediction(image: ciimage)
+            imagePrediction(image: reverseimage)
         }
+        
+        
         
         dismiss(animated: true, completion: nil)
     }
@@ -126,15 +106,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    
-    
     func imageFromARGB32Bitmap(pixels: [PixelData], width: Int, height: Int) -> UIImage? {
+        
         guard width > 0 && height > 0 else { return nil }
         guard pixels.count == width * height else { return nil }
 
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
         let bitsPerComponent = 8
+        //4byte
         let bitsPerPixel = 32
 
         var data = pixels // Copy to mutable []
@@ -160,37 +140,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         return UIImage(cgImage: cgim)
     }
-    
-    //    func RBGImage(data: [UInt8], width: Int, height: Int) -> CGImage? {
-    //
-    //        let bitsPerComponent = 8
-    //        let numberOfComponents = 3
-    //        let bitsPerPixel = bitsPerComponent * numberOfComponents
-    //        let bytesPerPixel = bitsPerPixel / 8
-    //
-    //        guard width > 0, height > 0 else { return nil }
-    //        guard width * height * numberOfComponents == data.count else { return nil }
-    //
-    //        return CGDataProvider(dataInfo: nil, data: data, size: data.count) { _,_,_ in }
-    //            .flatMap {
-    //                CGImage(width: width,
-    //                        height: height,
-    //                        bitsPerComponent: bitsPerComponent,
-    //                        bitsPerPixel: bitsPerPixel,
-    //                        bytesPerRow: width * bytesPerPixel,
-    //                        space: CGColorSpaceCreateDeviceRGB(),
-    //                        bitmapInfo: [],
-    //                        provider: $0,
-    //                        decode: nil,
-    //                        shouldInterpolate: false,
-    //                        intent: .defaultIntent)
-    //        }
-    //    }
-    
-    
 }
 
 extension UIImage {
+    
     //画像を指定のサイズにリサイズする（参考サイトのコードそのまま）。
     func resize(to newSize: CGSize) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(CGSize(width: newSize.width, height: newSize.height), true, 1.0)
@@ -202,7 +155,7 @@ extension UIImage {
     
     //画像をpixelBufferに変換。ただし参考サイトのコードを一部変更して、
     //ピクセル値を二値化はせずに0.0〜1.0の値として扱う（元々のモデルに合わせる）
-    func getPixelBuffer() -> [UInt8]
+    func getPixelBuffer() -> [PixelData]
     {
         guard let cgImage = self.cgImage else {
             return []
@@ -212,7 +165,8 @@ extension UIImage {
         let height = cgImage.height
         let bytesPerPixel = 4
         let pixelData = cgImage.dataProvider!.data! as Data
-        var buf : [UInt8] = []
+        
+        var pixels = [PixelData]()
         
         for j in 0..<height {
             for i in 0..<width {
@@ -220,17 +174,28 @@ extension UIImage {
                 let r = CGFloat(pixelData[pixelInfo])
                 let g = CGFloat(pixelData[pixelInfo+1])
                 let b = CGFloat(pixelData[pixelInfo+2])
+                let a = CGFloat(pixelData[pixelInfo+3])
                 
-                let v: Float = floor(Float(r + g + b)/3.0)
-                //                /255.0
-//                let v_int: Int = Int(v)
-                let v_uint: UInt8 = UInt8.init(v)
+                let r_uint: UInt8 = UInt8.init(r)
+                let g_uint: UInt8 = UInt8.init(g)
+                let b_uint: UInt8 = UInt8.init(b)
+                let a_uint: UInt8 = UInt8.init(a)
                 
-                buf.append(v_uint)
+                let uint_change: UInt8 = UInt8.init(Float(255))
+                
+                let r_uint_rev: UInt8 = uint_change - r_uint
+                let g_uint_rev: UInt8 = uint_change - g_uint
+                let b_uint_rev: UInt8 = uint_change - b_uint
+                let a_uint_rev: UInt8 = uint_change - a_uint
+                
+                print(r_uint, r_uint_rev)
+
+//                let red = PixelData(a: a_uint, r: r_uint, g: g_uint, b: b_uint)
+                let red = PixelData(a: 255, r: r_uint_rev, g: g_uint_rev, b: b_uint_rev)
+                pixels.append(red)
             }
         }
-        return buf
+        
+        return pixels
     }
-    
-    
 }
